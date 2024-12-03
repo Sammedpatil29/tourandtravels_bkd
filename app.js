df95711b9916315e9aa755f51b81f1bb5b64785f
Sammedpatil29 <108979063+Sammedpatil29@users.noreply.github.com>
2024-12-03 18:57:25 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const nodemailer = require('nodemailer')
+const bodyparser = require('body-parser')
+const cors = require('cors')
+
+const app = express()
+const port = 3000
+
+app.use(bodyparser.json())
+app.use(cors())
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'sammed.patil29@gmail.com',
+        pass: 'pypg oecr cmbu jusr',
+    },
+});
+
+app.post('/send-email', (req,res) => {
+    const {name, contact, place , message} = req.body;
+
+    const mailOptions = {
+        from: 'sammed.patil29@gmail.com',
+        to: 'sammed.patil29@gmail.com',
+        subject: 'New Request Submission',
+        text: `Name: ${name}\nContact: ${contact}\nPlace: ${place}\nMessage: ${message}`
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return res.status(500).json({ message: 'Error sending email', error });
+        } else {
+            return res.status(200).json({ message: 'Email sent successfully' });
+        }
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
